353f445cb7e98f2501e0e02f1d86075c382ac47b
merri <merrickshurtliff@gmail.com>
2023-11-17 13:33:19 -0700

    hmm

--- code/main.py
+++ code/main.py
@@ -1,10 +1,30 @@
 import pygame, sys
 from pytmx.util_pygame import load_pygame
 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self,pos,surf,groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
+
+
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 tmx_data = load_pygame('../Images/Maps/map1.tmx')
+sprite_group = pygame.sprite.Group()
 
+#Go throuh layers
+for layer in tmx_data.visible_layers:
+    # if layer.name in ('Floor', 'Plants and rocks', 'Pipes')
+     if hasattr(layer,'data'):
+         for x,y,surf in layer.tiles():
+            pos = (x * 128, y*128)
+            Tile(pos = pos, surf = surf, groups = sprite_group)
+
+for obj in tmx_data.objects:
+    pos = (obj.x, obj.y)
+    if obj.type:
+        Tile(pos = pos, surf=obj.image, groups = sprite_group)
 # print(tmx_data.layers)
 #
 # for layer in tmx_data.visible_layers:
@@ -27,10 +47,10 @@ tmx_data = load_pygame('../Images/Maps/map1.tmx')
 # print(layer.id)
 
 # get objects
-object_layer = tmx_data.get_layer_by_name('Object Layer 1')
-for obj in object_layer:
-    if obj.type == "Building":
-        print(obj)
+# object_layer = tmx_data.get_layer_by_name('Object Layer 1')
+# for obj in object_layer:
+#     if obj.type == "Building":
+#         print(obj)
 
 
 # for obj in tmx_data.objects:
@@ -47,4 +67,14 @@ while True:
             sys.exit
 
         screen.fill('black')
+        sprite_group.draw(screen)
+        
+        for obj in tmx_data.objects:
+            pos = (obj.x, obj.y)
+            if obj.type == 'Shape':
+                if obj.name == 'Marker':
+                    pygame.draw.circ.e(screen, 'red',(obj.x,obj.y),5)
+                if obj.name == 'Rectangle':
+                    rect = pygame.Rect(obj.x,obj.y,obj.width,obj.height)
+                    pygame.draw.rect(screen,'yellow',rect)
         pygame.display.update()
