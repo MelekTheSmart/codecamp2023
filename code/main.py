42b71cb8fb4bc0d6f6a484c837de72d845299ab1
merri <merrickshurtliff@gmail.com>
2023-11-17 17:23:32 -0700

    quick backup

--- code/main.py
+++ code/main.py
@@ -1,18 +1,75 @@
 import pygame, sys
 from pytmx.util_pygame import load_pygame
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self,pos,group):
+        super().__init__(group)
+        self.image = pygame.image.load('../Images/Objects/mushroom_3.png')
+        self.rect = self.image.get_rect(center = pos)
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+        rectangle = self.rect
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.direction.y = -1
+        elif keys[pygame.K_s]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_a]:
+            self.direction.x = -1
+        elif keys[pygame.K_d]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def update(self):
+        self.input()
+        future = pygame.Rect([self.rect.x, self.rect.y, 64,64])
+        future.center += self.direction * self.speed
+        if not checkbounds(future):
+            self.rect.center += self.direction * self.speed
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self):
+        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+            screen.blit(sprite.image,sprite.rect)
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self,pos,surf,groups):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
-
-
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 tmx_data = load_pygame('../Images/Maps/map1.tmx')
+clock = pygame.time.Clock()
 sprite_group = pygame.sprite.Group()
-
+camera_group = CameraGroup()
+MOVEMENTSPEED = 5
+def checkbounds(playerrec):
+    check = False
+    if (playerrec.collidelistall(tiles)): #this tests every tile with the player rectangle
+        check = True
+    return check
+tiles = []
+collision = tmx_data.get_layer_by_name('Collisions')
+for x, y, tile in collision:
+    if tile:
+        tiles.append(pygame.Rect([(x*128), (y*128), 128, 128]));
+# camera_group = pygame.sprite.Group()
 #Go throuh layers
 for layer in tmx_data.visible_layers:
     # if layer.name in ('Floor', 'Plants and rocks', 'Pipes')
@@ -23,8 +80,8 @@ for layer in tmx_data.visible_layers:
 
 for obj in tmx_data.objects:
     pos = (obj.x, obj.y)
-    if obj.type:
-        Tile(pos = pos, surf=obj.image, groups = sprite_group)
+    if obj.image:
+        Tile(pos = pos, surf=obj.image, groups = camera_group)
 # print(tmx_data.layers)
 #
 # for layer in tmx_data.visible_layers:
@@ -59,22 +116,34 @@ for obj in tmx_data.objects:
 #     # print(obj.image)
 #     if obj.type() == 'Building':
 #         print(obj)
-
+player = Player((100,100),camera_group)
 while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit
-
-        screen.fill('black')
+        # print(player.rect)
+        if checkbounds(player.rect):
+            print("help")
+        sprite_group.update()
         sprite_group.draw(screen)
-        
+        camera_group.update()
+        camera_group.custom_draw()
         for obj in tmx_data.objects:
             pos = (obj.x, obj.y)
             if obj.type == 'Shape':
                 if obj.name == 'Marker':
-                    pygame.draw.circ.e(screen, 'red',(obj.x,obj.y),5)
+                    pygame.draw.circle(screen, 'red',(obj.x,obj.y),5)
                 if obj.name == 'Rectangle':
                     rect = pygame.Rect(obj.x,obj.y,obj.width,obj.height)
                     pygame.draw.rect(screen,'yellow',rect)
+                if obj.name == 'Elipse':
+                    rect = pygame.Rect(obj.x,obj.y,obj.width,obj.height)
+                    pygame.draw.ellipse(screen, 'blue', rect)
+                if obj.name == 'Polygon':
+                    points = [(point.x,point.y) for point in obj.points]
+                    pygame.draw.polygon(screen,'green',points)
+
         pygame.display.update()
+        clock.tick(60)
