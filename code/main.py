da07675e095ab7e558dd0e54954051cee263d7bb
merri <merrickshurtliff@gmail.com>
2023-11-17 21:12:36 -0700

    backing up

--- code/main.py
+++ code/main.py
@@ -2,18 +2,91 @@ import pygame, sys
 from pytmx.util_pygame import load_pygame
 
 cooldown = 0
+class SpriteSheet:
+
+    def __init__(self, filename):
+        """Load the sheet."""
+        try:
+            self.sheet = pygame.image.load(filename).convert_alpha()
+        except pygame.error as e:
+            print(f"Unable to load spritesheet image: {filename}")
+            raise SystemExit(e)
+
+
+    def image_at(self, rectangle, colorkey = None):
+        """Load a specific image from a specific rectangle."""
+        # Loads image from x, y, x+offset, y+offset.
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size,pygame.SRCALPHA).convert_alpha()
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0,0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image
+
+    def images_at(self, rects, colorkey = None):
+        """Load a whole bunch of images and return them as a list."""
+        return [self.image_at(rect, colorkey) for rect in rects]
+
+    def load_strip(self, rect, image_count, colorkey = None):
+        """Load a whole strip of images, and return them as a list."""
+        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey)
+
+    def load_grid_images(self, num_rows, num_cols, x_margin=0, x_padding=0,
+            y_margin=0, y_padding=0):
+        """Load a grid of images.
+        x_margin is space between top of sheet and top of first row.
+        x_padding is space between rows.
+        Assumes symmetrical padding on left and right.
+        Same reasoning for y.
+        Calls self.images_at() to get list of images.
+        """
+        sheet_rect = self.sheet.get_rect()
+        sheet_width, sheet_height = sheet_rect.size
+
+        # To calculate the size of each sprite, subtract the two margins,
+        #   and the padding between each row, then divide by num_cols.
+        # Same reasoning for y.
+        x_sprite_size = ( sheet_width - 2 * x_margin
+                - (num_cols - 1) * x_padding ) / num_cols
+        y_sprite_size = ( sheet_height - 2 * y_margin
+                - (num_rows - 1) * y_padding ) / num_rows
+
+        sprite_rects = []
+        for row_num in range(num_rows):
+            for col_num in range(num_cols):
+                # Position of sprite rect is margin + one sprite size
+                #   and one padding size for each row. Same for y.
+                x = x_margin + col_num * (x_sprite_size + x_padding)
+                y = y_margin + row_num * (y_sprite_size + y_padding)
+                sprite_rect = (x, y, x_sprite_size, y_sprite_size)
+                sprite_rects.append(sprite_rect)
+
+        grid_images = self.images_at(sprite_rects)
+        print(f"Loaded {len(grid_images)} grid images.")
+
+        return grid_images
+
+
 class Player(pygame.sprite.Sprite):
+
     def __init__(self,pos,group):
         super().__init__(group)
-        # self.image = pygame.image.load('../Images/Objects/mushroom_3.png')
-        self.image = pygame.image.load('../Images/Objects/New Piskel-1.png.png')
+        self.images = [SpriteSheet('../Images/Sprites/beeeiigcube.png').load_grid_images(4, 1, x_margin=0,
+                x_padding=0, y_margin=1, y_padding=0), SpriteSheet('../Images/Sprites/SmoolCube.png').load_grid_images(4, 1, x_margin=2,
+                x_padding=0, y_margin=1, y_padding=0)]
+        self.image = self.images[1][1]
         self.rect = self.image.get_rect(center = pos)
         self.direction = pygame.math.Vector2()
         self.speed = 5
         self.toggle = True
         self.togglesize = False
-        self.size = 32
-
+        self.size = 16
+        self.cubesize = 1
+        self.cubecycle = 0
         rectangle = self.rect
 
 
@@ -42,20 +115,22 @@ class Player(pygame.sprite.Sprite):
             self.speed = 5
         if keys[pygame.K_b]:
             centerpos = (self.rect.center)
-            if not self.togglesize and cooldown>=0:
-                self.image = pygame.image.load('../Images/Objects/New Piskel-1.png (2).png')
+            if not self.togglesize and cooldown==0:
+                # self.image = self.images[0][1]
                 self.rect = self.image.get_rect(center=centerpos)
                 self.size = 64
                 self.togglesize = True
                 cooldown = 10
                 self.speed = 5
-            elif self.togglesize and cooldown>=0:
-                self.image = pygame.image.load('../Images/Objects/New Piskel-1.png.png')
+                self.cubesize = 0
+            elif self.togglesize and cooldown==0:
+                # self.image = self.images[1][1]
                 self.rect = self.image.get_rect(center=centerpos)
                 self.size = 32
                 self.togglesize = False
                 cooldown = 10
                 self.speed = 10
+                self.cubesize = 1
         # if keys[pygame.K_v]:
         #     self.image = pygame.image.load('../Images/Objects/New Piskel-1.png.png')
         #     self.size = 32
@@ -72,11 +147,12 @@ class CameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
-
         self.offset = pygame.math.Vector2()
 
     def custom_draw(self):
         for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+            # player.cubesize = 1
+            player.image = player.images[player.cubesize][player.cubecycle]
             screen.blit(sprite.image,sprite.rect)
 
 class Tile(pygame.sprite.Sprite):
@@ -92,6 +168,7 @@ sprite_group = pygame.sprite.Group()
 camera_group = CameraGroup()
 MOVEMENTSPEED = 5
 health = 2
+count = 0
 def checkbounds(playerrec):
     check = False
     if (playerrec.collidelistall(tiles)): #this tests every tile with the player rectangle
@@ -159,26 +236,41 @@ while True:
         # print(player.rect)
         if checkbounds(player.rect):
             print("help")
-        sprite_group.update()
-        sprite_group.draw(screen)
-        camera_group.update()
-        camera_group.custom_draw()
-        for obj in tmx_data.objects:
-            pos = (obj.x, obj.y)
-            if obj.type == 'Shape':
-                if obj.name == 'Marker':
-                    pygame.draw.circle(screen, 'red',(obj.x,obj.y),5)
-                if obj.name == 'Rectangle':
-                    rect = pygame.Rect(obj.x,obj.y,obj.width,obj.height)
-                    pygame.draw.rect(screen,'yellow',rect)
-                if obj.name == 'Elipse':
-                    rect = pygame.Rect(obj.x,obj.y,obj.width,obj.height)
-                    pygame.draw.ellipse(screen, 'blue', rect)
-                if obj.name == 'Polygon':
-                    points = [(point.x,point.y) for point in obj.points]
-                    pygame.draw.polygon(screen,'green',points)
-        if cooldown > 0:
-            cooldown = cooldown - 1
+    sprite_group.update()
+    sprite_group.draw(screen)
+    camera_group.update()
+    camera_group.custom_draw()
+    for obj in tmx_data.objects:
+        pos = (obj.x, obj.y)
+        if obj.type == 'Shape':
+            if obj.name == 'Marker':
+                pygame.draw.circle(screen, 'red',(obj.x,obj.y),5)
+            if obj.name == 'Rectangle':
+                rect = pygame.Rect(obj.x,obj.y,obj.width,obj.height)
+                pygame.draw.rect(screen,'yellow',rect)
+            if obj.name == 'Elipse':
+                rect = pygame.Rect(obj.x,obj.y,obj.width,obj.height)
+                pygame.draw.ellipse(screen, 'blue', rect)
+            if obj.name == 'Polygon':
+                points = [(point.x,point.y) for point in obj.points]
+                pygame.draw.polygon(screen,'green',points)
+
+    if cooldown > 0:
+        cooldown = cooldown - 1
         # print(cooldown)
-        pygame.display.update()
-        clock.tick(120)
+    pygame.display.update()
+    if count == 5:
+        if player.cubecycle == 0:
+            player.cubecycle = 1
+        elif player.cubecycle == 1:
+            player.cubecycle = 2
+        elif player.cubecycle == 2:
+            player.cubecycle = 3
+        elif player.cubecycle == 3:
+            player.cubecycle = 0
+        print (player.cubesize)
+    if count == 20:
+        count = 0
+    else:
+        count += 1
+        clock.tick(30)
